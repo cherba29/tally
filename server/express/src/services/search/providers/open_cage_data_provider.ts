e7d41e31dc511955ef9398a7255e6121f070e17f
cherba29 <arthur@cherba.org>
2020-09-04 15:56:32 -0400

    Add Unit test support via Jest and a unit test.

--- server/express/src/services/search/providers/open_cage_data_provider.ts
+++ server/express/src/services/search/providers/open_cage_data_provider.ts
@@ -1,8 +1,8 @@
-import request from "request-promise";
+import axios from 'axios';
 
 export const getPlaces = async (query: string) => {
   const key = process.env.OPEN_CAGE_DATA_KEY;
   const url = `https://api.opencagedata.com/geocode/v1/geojson?q=${query}&key=${key}&limit=20&no_annotations=1`;
-  const response = await request(url);
-  return JSON.parse(response);
+  const response = await axios.get(url);
+  return response.data;
 };
