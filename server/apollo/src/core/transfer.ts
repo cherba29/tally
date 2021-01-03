ce45813ce594495171ada31f62fcbf446ebb335b
cherba29 <arthur@cherba.org>
2021-01-03 10:06:56 -0500

    Add missing definition of Transfer.

---
+++ server/apollo/src/core/transfer.ts
@@ -0,0 +1,12 @@
+import { Account } from "./account";
+import { Balance } from "./balance";
+import { Month } from "./month";
+
+export interface Transfer {
+  fromAccount: Account;
+  toAccount: Account;
+  fromMonth: Month;
+  toMonth: Month;
+  description: string;
+  balace: Balance;
+}
