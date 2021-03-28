84f24e35dd0fb8ea98519835a589f399964e83ef
cherba29 <arthur@cherba.org>
2021-03-28 18:49:48 -0400

    Add core balance and account tests.

--- server/apollo/src/core/account.test.ts
+++ server/apollo/src/core/account.test.ts
@@ -1,11 +1,117 @@
 import {Account, Type} from './account';
 import {Month} from './month';
 
-test('Account', () => {
+test('isClosed - by default', () => {
   const account = new Account({
     name: 'testAccount',
     type: Type.CHECKING,
     owners: ['bob'],
   });
   expect(account.isClosed(new Month(2021, 2))).toBe(true);
+});
+
+test('isClosed false if closedOn not set', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['bob'],
+    openedOn: new Month(2021, 1),
+  });
+  expect(account.isClosed(new Month(2021, 2))).toBe(false);
+});
+
+test('isClosed true if closedOn is set', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['bob'],
+    openedOn: new Month(2021, 1),
+    closedOn: new Month(2021, 3),
+  });
+  expect(account.isClosed(new Month(2021, 4))).toBe(true);
+});
+
+test('isSummary false if type is not summary', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['bob'],
+  });
+  expect(account.isSummary).toBe(false);
+});
+
+test('isSummary when type is summary', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.SUMMARY,
+    owners: ['bob'],
+  });
+  expect(account.isSummary).toBe(true);
+});
+
+test('has common owner is false if no common owners', () => {
+  const account1 = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['bob'],
+  });
+  const account2 = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['john'],
+  });
+
+  expect(account1.hasCommonOwner(account2)).toBe(false);
+});
+
+test('has common owner is true if common owners', () => {
+  const account1 = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['bob'],
+  });
+  const account2 = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['john', 'bob'],
+  });
+
+  expect(account1.hasCommonOwner(account2)).toBe(true);
+});
+
+
+test('isExternal false by default', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.CHECKING,
+    owners: ['bob'],
+  });
+  expect(account.isExternal).toBe(false);
+});
+
+test('isExternal true when type is EXTERNAL', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.EXTERNAL,
+    owners: ['bob'],
+  });
+  expect(account.isExternal).toBe(true);
+});
+
+test('isExternal true when type is TAX', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.TAX,
+    owners: ['bob'],
+  });
+  expect(account.isExternal).toBe(true);
+});
+
+test('isExternal true when type is DEFFERED_INCOME', () => {
+  const account = new Account({
+    name: 'testAccount',
+    type: Type.DEFERRED_INCOME,
+    owners: ['bob'],
+  });
+  expect(account.isExternal).toBe(true);
 });
---
+++ server/apollo/src/core/balance.test.ts
@@ -0,0 +1,37 @@
+import {Balance, Type} from './balance';
+
+
+test('negation', () => {
+  const balance = new Balance(100.0, new Date(2020, 1, 3), Type.CONFIRMED);
+  const negated = Balance.negated(balance);
+  expect(negated.date).toEqual(new Date(2020, 1, 3));
+  expect(negated.amount).toBe(-100.0);
+  expect(negated.type).toBe(Type.CONFIRMED);
+});
+
+test('addition - same type', () => {
+  const balance1 = new Balance(100.0, new Date(2020, 1, 3), Type.CONFIRMED);
+  const balance2 = new Balance(200.0, new Date(2020, 2, 3), Type.CONFIRMED);
+  const sum = Balance.add(balance1, balance2);
+  expect(sum.date).toEqual(new Date(2020, 2, 3));
+  expect(sum.amount).toBe(300.0);
+  expect(sum.type).toBe(Type.CONFIRMED);
+});
+
+test('addition - different type', () => {
+  const balance1 = new Balance(100.0, new Date(2020, 1, 3), Type.CONFIRMED);
+  const balance2 = new Balance(200.0, new Date(2020, 0, 3), Type.PROJECTED);
+  const sum = Balance.add(balance1, balance2);
+  expect(sum.date).toEqual(new Date(2020, 1, 3));
+  expect(sum.amount).toBe(300.0);
+  expect(sum.type).toBe(Type.PROJECTED);
+});
+
+test('subtraction', () => {
+  const balance1 = new Balance(100.0, new Date(2020, 1, 3), Type.CONFIRMED);
+  const balance2 = new Balance(200.0, new Date(2020, 0, 3), Type.PROJECTED);
+  const sum = Balance.subtract(balance1, balance2);
+  expect(sum.date).toEqual(new Date(2020, 1, 3));
+  expect(sum.amount).toBe(-100.0);
+  expect(sum.type).toBe(Type.PROJECTED);
+});
