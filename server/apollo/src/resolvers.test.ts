a1e1f2ed1728e2477821859177a6c6d4d49d7e89
cherba29 <arthur@cherba.org>
2021-04-11 11:49:06 -0400

    Start resolvers module unit tests.

---
+++ server/apollo/src/resolvers.test.ts
@@ -0,0 +1,25 @@
+import { Month } from './core/month';
+import resolvers from './resolvers';
+
+describe('date', () => {
+  test('parseValue', () => {
+    expect(resolvers.Date.parseValue('2021-03-05')).toEqual(new Date('2021-03-05'));
+  });
+});
+
+describe('GqlMonth', () => {
+  test('parseValue', () => {
+    expect(resolvers.GqlMonth.parseValue('Mar2021')).toEqual(new Month(2021, 2));
+  });
+});
+
+describe('query', () => {
+  test('buildBudget', () => {
+    expect(resolvers.Query.budget).toThrow(
+      new Error('Process environment variable "TALLY_FILES" has not been specified.'));
+  });
+
+  afterEach(() => {
+    delete process.env.TALLY_FILES;
+  });
+});
