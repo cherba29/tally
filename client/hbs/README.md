1a05e6ebeee4a371eaa58db76015c3850c987123
cherba29 <arthur@cherba.org>
2021-08-01 09:23:04 -0400

    [client] Switch test framework from jasmine to jest.

---
+++ client/hbs/README.md
@@ -0,0 +1,26 @@
+# Tally client side
+
+WARNING: This app is not secure, it allows one to load yaml financial data
+from any location. Do not expose it online.
+
+## Setup
+
+To build js bundle use.
+```
+npm run build
+```
+
+It is loaded by `js_summary.html`. Java `jetty` server is also a
+file server, opening this file will start the app.
+
+`js_summary.html` requires query hash parameter, for example
+
+```
+http://localhost:8080/files/client/hbs/js_summary.html#../../../data/yaml/files
+```
+
+## Development
+
+```
+npm run test
+```
--- client/hbs/js/utils_test.ts
+++ client/hbs/js/utils_test.ts
@@ -4,11 +4,11 @@ import { transformBudgetData } from './utils';
 describe("transformBudgetData", function () {
 
   it("works on empty", function () {
-    let months: string[] = [];
-    let accountNameToAccount = {};
-    let statements = {};
-    let summaries = {};
-    let data_view = transformBudgetData(
+    const months: string[] = [];
+    const accountNameToAccount = {};
+    const statements = {};
+    const summaries = {};
+    const data_view = transformBudgetData(
         months, accountNameToAccount, statements, summaries);
 
     expect(data_view).toEqual({
@@ -19,42 +19,40 @@ describe("transformBudgetData", function () {
   });
 
   it("works on single", function () {
-    let months = ["Sep2014"];
-    let accountNameToAccount: {[accountName:string]: TallyAccount} = {
-      main: {
-        name: "main",
-        address: "some place",
-        closedOn: null,
-        description: "Main account",
-        external: false,
-        number: "123",
-        openedOn: "Aug2014",
-        owners: ["owner1", "owner2"],
-        password: "password",
-        phone: "123-456-7890",
-        summary: false,
-        type: "credit",
-        url: "http://somewhere",
-        userName: "user1"
-      }
+    const months = ["Sep2014"];
+    const account: TallyAccount = {
+      name: "main",
+      address: "some place",
+      closedOn: null,
+      description: "Main account",
+      external: false,
+      number: "123",
+      openedOn: "Aug2014",
+      owners: ["owner1", "owner2"],
+      password: "password",
+      phone: "123-456-7890",
+      summary: false,
+      type: "credit",
+      url: "http://somewhere",
+      userName: "user1"
     };
-    let statements: {[accountName:string]: {[month:string]: Statement}} = {
-      main: {
-        Sep2014: {
-          isClosed: true,
-          addSub: 100,
-          endBalance: {
-            amount: 3000,
-            type: 'PROJECTED',
-          },
-          isCovered: true,
-          isProjectedCovered: true,
-          hasProjectedTransfer: true,
-          percentChange: 1.4,
-          unaccounted: 500,
-          transactions: []
-        },
-      }
+    const accountNameToAccount: {[accountName:string]: TallyAccount} = { main: account };
+    const statement: Statement =  {
+      isClosed: true,
+      addSub: 100,
+      endBalance: {
+        amount: 3000,
+        type: 'PROJECTED',
+      },
+      isCovered: true,
+      isProjectedCovered: true,
+      hasProjectedTransfer: true,
+      percentChange: 1.4,
+      unaccounted: 500,
+      transactions: []
+    };
+    const statements: {[accountName:string]: {[month:string]: Statement}} = {
+      main: { Sep2014: statement }
     };
     let summaries: {[ownerAccountType:string]: {[month:string]: SummaryStatement}} = {
       'owner1 credit': {
