24171b2a18722c8591b8013b50cf351b32db3fe9
cherba29 <arthur@cherba.org>
2023-06-02 08:58:39 -0400

    Remove jetty support.

--- server/jetty/src/main/java/tally/core/BudgetException.java
+++
@@ -1,9 +0,0 @@
-package tally.core;
-
-public class BudgetException extends Exception {
-  private static final long serialVersionUID = 1L;
-
-  public BudgetException(String message) {
-    super(message);
-  }
-}
--- server/jetty/src/main/java/tally/core/MissDatedBalanceException.java
+++
@@ -1,9 +0,0 @@
-package tally.core;
-
-public class MissDatedBalanceException extends BudgetException {
-  private static final long serialVersionUID = 1L;
-
-  public MissDatedBalanceException(String message) {
-    super(message);
-  }
-}
