64d2ee34d59667e12baaefe3e8661996d4c382b9
cherba29 <arthur@cherba.org>
2023-07-06 15:13:53 -0400

    Render n/a as dash.

--- client/hbs/src/format.ts
+++ client/hbs/src/format.ts
@@ -1,3 +1,4 @@
+import {html, TemplateResult} from 'lit';
 import {Type as BalanceType} from '@tally/lib/core/balance';
 import {GqlBalance} from './gql_types';
 
@@ -11,11 +12,11 @@ export function dateFormat(value: string | Date | undefined | null): string {
   return value.toISOString().slice(0, 10);
 }
 
-export function currency(value: number | undefined | null): string {
+export function currency(value: number | undefined | null): TemplateResult {
   if (value === null || value === undefined) {
-    return 'n/a';
+    return html`&mdash;`;
   }
-  return (value / 100.0).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+  return html`${(value / 100.0).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}`;
 }
 
 export function isProjected(balance: GqlBalance | undefined | null): boolean {
