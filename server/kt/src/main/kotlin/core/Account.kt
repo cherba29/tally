5b48a446d817a0a2b7e6c401894e653be71dd7b4
cherba29 <arthur@cherba.org>
2026-02-18 07:49:32 -0500

    kt server: Add Account and its tests.

---
+++ server/kt/src/main/kotlin/core/Account.kt
@@ -0,0 +1,55 @@
+package com.cherba29.tally.core
+
+enum class Type(val id: String) {
+  UNSPECIFIED("_unspecified_"),
+  BILL("bill"),
+  CHECKING("checking"),
+  CREDIT("credit"),
+  CREDIT_CARD("credit-card"),
+  DEFERRED_INCOME("deferred income"),
+  EXTERNAL("external"),
+  INCOME("income"),
+  INVESTMENT("investment"),
+  RETIREMENT("retirement"),
+  SUMMARY("_summary_"),
+  TAX("tax_"),
+}
+
+data class Account(
+  val name: String,
+  val description: String? = null,
+  val path: List<String> = listOf(),
+  // Account type, for example 'CREDIT_CARD'.
+  val type: Type,
+  // Real account number associated with this account.
+  val number: String? = null,
+  // Month when account was opened and possibly closed.
+  val openedOn: Month? = null,
+  val closedOn: Month? = null,
+  // List of account owner ids.
+  val owners: List<String> = listOf(),
+  // Url to the account.
+  val url: String? = null,
+  // Physical address for the account.
+  val address: String? = null,
+  // Phone number for customer support.
+  val phone: String? = null,
+  // Username/password to use to login to the account.
+  val userName: String? = null,
+  val password: String? = null,
+) {
+  override fun toString(): String = "Account $name ${type.id}${if (closedOn == null) "" else " Closed $closedOn"}"
+
+  fun isClosed(month: Month): Boolean {
+    return (closedOn != null) && (closedOn < month) || // After closed.
+           (openedOn != null) && (month < openedOn) // Before or on open.
+  }
+
+  val isExternal: Boolean = type === Type.EXTERNAL || type === Type.TAX || type === Type.DEFERRED_INCOME
+
+  val isSummary: Boolean = type == Type.SUMMARY
+
+  fun hasCommonOwner(other: Account): Boolean = owners.intersect(other.owners).isNotEmpty()
+
+  val typeIdName = type.name
+}
---
+++ server/kt/src/test/kotlin/core/AccountTest.kt
@@ -0,0 +1,135 @@
+package com.cherba29.tally.core
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class AccountTest : DescribeSpec({
+  describe("open/closed") {
+    it("isClosed - false by default") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+      )
+      account.isClosed(Month(2021, 2)) shouldBe false
+    }
+
+    it("isClosed false if closedOn not set") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+        openedOn = Month(2021, 1),
+      )
+      account.isClosed(Month(2021, 2)) shouldBe false
+    }
+
+    it("isClosed true if closedOn is set") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+        openedOn = Month(2021, 1),
+        closedOn = Month(2021, 3),
+      )
+      account.isClosed(Month(2021, 4)) shouldBe true
+    }
+  }
+  describe("type") {
+    it("isSummary false if type is not summary") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+      )
+      account.isSummary shouldBe false
+    }
+
+    it("isSummary when type is summary") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.SUMMARY,
+        owners = listOf("bob"),
+      )
+      account.isSummary shouldBe true
+    }
+
+    it("isExternal false by default") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+      )
+      account.isExternal shouldBe false
+    }
+
+    it("isExternal true when type is EXTERNAL") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.EXTERNAL,
+        owners = listOf("bob"),
+      )
+      account.isExternal shouldBe true
+    }
+
+    it("isExternal true when type is TAX") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.TAX,
+        owners = listOf("bob"),
+      )
+      account.isExternal shouldBe true
+    }
+
+    it("isExternal true when type is DEFERRED_INCOME") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.DEFERRED_INCOME,
+        owners = listOf("bob"),
+      )
+      account.isExternal shouldBe true
+    }
+  }
+  describe("owner") {
+    it("has common owner is false if no common owners") {
+      val account1 = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+      )
+      val account2 = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("john'"),
+      )
+
+      account1.hasCommonOwner(account2) shouldBe false
+    }
+
+    it("has common owner is true if common owners") {
+      val account1 = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("bob"),
+      )
+      val account2 = Account(
+        name = "testAccount",
+        type = Type.CHECKING,
+        owners = listOf("john", "bob"),
+      )
+
+      account1.hasCommonOwner(account2) shouldBe true
+    }
+  }
+
+  describe("conversions") {
+    it("toString") {
+      val account = Account(
+        name = "testAccount",
+        type = Type.TAX,
+        owners = listOf("bob"),
+      )
+      account.toString() shouldBe "Account testAccount tax_"
+    }
+  }
+})
