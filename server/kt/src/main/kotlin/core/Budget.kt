860133fd6bd0f32d9ed8258ef94f7e4bfd986d4c
cherba29 <arthur@cherba.org>
2026-02-19 09:02:39 -0500

    kt server: add Budget class and its tests.

---
+++ server/kt/src/main/kotlin/core/Budget.kt
@@ -0,0 +1,151 @@
+package com.cherba29.tally.core
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+data class Budget(
+  // Period over which the budget is defined.
+  val months: List<Month>,
+  // Account name to account map.
+  val accounts: Map<String, Account>,
+  // Account name -> month -> balance map.
+  val balances: Map<String, Map<String, Balance>>,
+  // Account name -> month -> transfers map.
+  val transfers: Map<String, Map<String, Set<Transfer>>>
+) {
+  fun findActiveAccounts(): List<Account> {
+    val activeAccounts = mutableListOf<Account>()
+    for (account in accounts.values) {
+      if (months.find { m -> !account.isClosed(m) } != null) {
+        activeAccounts.add(account)
+      }
+    }
+    return activeAccounts
+  }
+}
+
+data class TransferData(
+  val toAccount: String,
+  val toMonth: Month,
+  val fromAccount: String,
+  val fromMonth: Month,
+  val balance: Balance,
+  val description: String?,
+)
+
+private fun <T> getMonthTransfers(
+  transfers: MutableMap<String, MutableMap<String, MutableSet<T>>>,
+  accountName: String,
+  month: String
+): MutableSet<T> {
+  var accountTransfers = transfers[accountName]
+  if (accountTransfers == null) {
+    accountTransfers = mutableMapOf()
+    transfers[accountName] = accountTransfers
+  }
+  var monthTransfers = accountTransfers[month]
+  if (monthTransfers == null) {
+    monthTransfers = mutableSetOf()
+    accountTransfers[month] = monthTransfers
+  }
+  return monthTransfers
+}
+
+class BudgetBuilder(
+  private var minMonth: Month? = null,
+  private var maxMonth: Month? = null,
+  // Account name to account map.
+  val accounts: MutableMap<String, Account> = mutableMapOf(),
+  // Account name -> month -> balance map.
+  val balances: MutableMap<String, MutableMap<String, Balance>> = mutableMapOf(),
+  val transfers: MutableList<TransferData> = mutableListOf()
+) {
+  fun setAccount(account: Account) {
+    accounts[account.name] = account
+    if (account.openedOn != null) {
+      minMonth = if (minMonth != null) Month.min(minMonth!!, account.openedOn) else account.openedOn
+      maxMonth = if (maxMonth != null) Month.max(maxMonth!!, account.openedOn) else account.openedOn
+    }
+    if (account.closedOn != null) {
+      minMonth = if (minMonth != null) Month.min(minMonth!!, account.closedOn) else account.closedOn
+      maxMonth = if (maxMonth != null) Month.max(maxMonth!!, account.closedOn) else account.closedOn
+    }
+  }
+
+  fun setBalance(accountName: String, month: String, balance: Balance) {
+    var accountBalances = balances[accountName]
+    if (accountBalances == null) {
+      accountBalances = mutableMapOf()
+      balances[accountName] = accountBalances
+    }
+    val existingBalance = accountBalances[month]
+    if (existingBalance != null) {
+      throw IllegalArgumentException("Balance for '$accountName' '$month' is already set to $balance")
+    }
+    accountBalances[month] = balance
+    val parsedMonth = Month.fromString(month)
+    minMonth = if (minMonth != null) Month.min(minMonth!!, parsedMonth) else parsedMonth
+    maxMonth = if (maxMonth != null) Month.max(maxMonth!!, parsedMonth) else parsedMonth
+  }
+
+  fun addTransfer(transferData: TransferData) {
+    transfers.add(transferData)
+    minMonth = Month.min(
+      minMonth ?: transferData.toMonth,
+      transferData.toMonth,
+      transferData.fromMonth
+    )
+    this.maxMonth = Month.max(
+      maxMonth ?: transferData.toMonth,
+      transferData.toMonth,
+      transferData.fromMonth
+    )
+  }
+
+  fun build(): Budget {
+    val budgetTransfers: MutableMap<String, MutableMap<String, MutableSet<Transfer>>> = mutableMapOf()
+    for (transferData in transfers) {
+      val toAccount = accounts[transferData.toAccount] ?: throw IllegalArgumentException(
+        "Unknown account ${transferData.toAccount}"
+      )
+
+      val fromAccount = accounts[transferData.fromAccount] ?: throw IllegalArgumentException(
+        "Unknown account ${transferData.fromAccount}"
+      )
+
+      if (toAccount.owners.sorted().joinToString() != fromAccount.owners.sorted().joinToString()) {
+        logger.warn {
+          "WARNING: Transaction ${transferData.fromMonth} -> ${transferData.toMonth} has " +
+              "to account ${toAccount.name} from ${fromAccount.name} with different owners " +
+              "${toAccount.owners.joinToString()} vs ${fromAccount.owners.joinToString()}"
+        }
+      }
+      val transfer = Transfer(
+        fromAccount,
+        toAccount,
+        transferData.fromMonth,
+        transferData.toMonth,
+        transferData.description,
+        transferData.balance
+      )
+      val toMonthTransfers = getMonthTransfers(
+        budgetTransfers,
+        toAccount.name,
+        transferData.toMonth.toString()
+      )
+      toMonthTransfers.add(transfer)
+      val fromMonthTransfers = getMonthTransfers(
+        budgetTransfers,
+        fromAccount.name,
+        transferData.fromMonth.toString()
+      )
+      fromMonthTransfers.add(transfer)
+    }
+    val months =
+      if (minMonth != null && maxMonth != null) Month.generate(minMonth!!, maxMonth!!.next()).toList() else listOf()
+    return Budget(months, this.accounts, this.balances, budgetTransfers)
+  }
+
+  companion object {
+    private val logger = KotlinLogging.logger {}
+  }
+}
---
+++ server/kt/src/test/kotlin/core/BudgetTest.kt
@@ -0,0 +1,144 @@
+package com.cherba29.tally.core
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.datetime.LocalDate
+
+class BudgetTest : DescribeSpec({
+  it("build empty budget") {
+    val builder = BudgetBuilder()
+    val budget = builder.build()
+    budget.accounts.size shouldBe 0
+    budget.balances.size shouldBe 0
+    budget.months.size shouldBe 0
+    budget.transfers.size shouldBe 0
+  }
+
+  it("build simple") {
+    val builder = BudgetBuilder()
+    val account1 = Account(
+      name = "test-account1",
+      type = AccountType.EXTERNAL,
+      owners = listOf(),
+    )
+    val account2 = Account(
+      name = "test-account2",
+      type = AccountType.BILL,
+      owners = listOf(),
+    )
+    val account3 = Account(
+      name = "test-account3",
+      type = AccountType.BILL,
+      owners = listOf(),
+    )
+
+    builder.setAccount(account1)
+    builder.setAccount(account2)
+    builder.setAccount(account3)
+    builder.setBalance(
+      "test-account1",
+      "Nov2019",
+      Balance(100.0, LocalDate(2019, 11, 1), BalanceType.PROJECTED)
+    )
+    builder.setBalance(
+      "test-account1",
+      "Dec2019",
+      Balance(200.0, LocalDate(2019, 12, 1), BalanceType.PROJECTED)
+    )
+    builder.setBalance(
+      "test-account2",
+      "Nov2019",
+      Balance(200.0, LocalDate(2019, 11, 3), BalanceType.CONFIRMED)
+    )
+    builder.addTransfer(
+      TransferData(
+        toAccount = "test-account1",
+        toMonth = Month(2019, 10),
+        fromAccount = "test-account2",
+        fromMonth = Month(2019, 10),
+        balance = Balance(50.0, LocalDate(2019, 11, 2), BalanceType.CONFIRMED),
+        description = null
+      )
+    )
+    builder.addTransfer(
+      TransferData(
+        toAccount = "test-account3",
+        toMonth = Month(2019, 10),
+        fromAccount = "test-account2",
+        fromMonth = Month(2019, 10),
+        balance = Balance(70.0, LocalDate(2019, 11, 2), BalanceType.CONFIRMED),
+        description = null
+      )
+    )
+
+    val budget = builder.build()
+    budget.accounts.size shouldBe 3
+    builder.accounts["test-account1"] shouldBe account1
+    builder.accounts["test-account2"] shouldBe account2
+    budget.balances.size shouldBe 2
+    budget.months shouldBe listOf("Nov2019", "Dec2019").map { Month.fromString(it) }
+    budget.transfers.size shouldBe 3
+  }
+
+  it("build budget - duplicate balance") {
+    val builder = BudgetBuilder();
+    val account1 = Account(
+      name = "test-account1",
+      type = AccountType.EXTERNAL,
+      owners = listOf(),
+    )
+    builder.setAccount(account1)
+    builder.setBalance(
+      "test-account1",
+      "Nov2019",
+      Balance(100.0, LocalDate(2019, 11, 1), BalanceType.PROJECTED)
+    );
+    val exception = shouldThrow<IllegalArgumentException> {
+      builder.setBalance(
+        "test-account1",
+        "Nov2019",
+        Balance(200.0, LocalDate(2020, 3, 1), BalanceType.PROJECTED)
+      )
+    }
+    exception.message shouldBe "Balance for 'test-account1' 'Nov2019' is already set to Balance { amount: 200.00, date: 2020-03-01, type: PROJECTED }"
+  }
+
+  it("build budget - bad to account") {
+    val builder = BudgetBuilder();
+    builder.addTransfer(
+      TransferData(
+        toAccount = "test-account1",
+        toMonth = Month(2019, 10),
+        fromAccount = "test-account2",
+        fromMonth = Month(2019, 10),
+        balance = Balance(50.0, LocalDate(2019, 12, 2), BalanceType.CONFIRMED),
+        description = null,
+      )
+    )
+    val exception = shouldThrow<IllegalArgumentException> { builder.build() }
+    exception.message shouldBe "Unknown account test-account1"
+  }
+
+  it("build budget - bad from account") {
+    val builder = BudgetBuilder();
+    val account1 = Account(
+      name = "test-account1",
+      type = AccountType.EXTERNAL,
+      owners = listOf(),
+    )
+    builder.setAccount(account1);
+    builder.addTransfer(
+      TransferData(
+        toAccount = "test-account1",
+        toMonth = Month(2019, 10),
+        fromAccount = "test-account2",
+        fromMonth = Month(2019, 10),
+        balance = Balance(50.0, LocalDate(2019, 11, 2), BalanceType.CONFIRMED),
+        description = null,
+      )
+    )
+    val exception = shouldThrow<IllegalArgumentException> { builder.build() }
+    exception.message shouldBe "Unknown account test-account2"
+  }
+})
