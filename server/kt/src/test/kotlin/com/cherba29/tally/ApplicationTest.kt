e59a70e471075ea5ef7909fd7cbc044aa57f1bb2
arti <arthur@cherba.org>
2026-06-29 09:26:29 -0400

    kt server: Use selfie in ApplicationTest to snapshot graphql schema.

--- server/kt/src/test/kotlin/com/cherba29/tally/ApplicationTest.kt
+++ server/kt/src/test/kotlin/com/cherba29/tally/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package com.cherba29.tally
 
+import com.diffplug.selfie.coroutines.expectSelfie
 import com.expediagroup.graphql.server.types.GraphQLRequest
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.engine.spec.tempdir
@@ -44,7 +45,6 @@ fun ApplicationTestBuilder.configureTestClient(tallyPath: Path?) {
 }
 
 class ApplicationTest : DescribeSpec({
-  coroutineTestScope = true
   timeout = 15.seconds.toLong(DurationUnit.MILLISECONDS)
   val tallyPath = tempdir("tally-", keepOnFailure = false).toPath()
   ((tallyPath / "client").createDirectory() / "index.html").createFile().writeText("Hello World!")
@@ -104,182 +104,15 @@ class ApplicationTest : DescribeSpec({
     }
 
     it("SDL route test") {
-      val expectedSchema = """
-        "Schema for Tally data"
-        schema @contact(description : "Report issues on github.", name : "Tally", url : "https://github.com/cherba29/tally"){
-          query: Query
-        }
-
-        "Provides contact information of the owner responsible for this subgraph schema."
-        directive @contact(description: String!, name: String!, url: String!) on SCHEMA
-
-        "Marks the field, argument, input field or enum value as deprecated"
-        directive @deprecated(
-            "The reason for the deprecation"
-            reason: String = "No longer supported"
-          ) on FIELD_DEFINITION | ARGUMENT_DEFINITION | ENUM_VALUE | INPUT_FIELD_DEFINITION
-
-        "Directs the executor to include this field or fragment only when the `if` argument is true"
-        directive @include(
-            "Included when true."
-            if: Boolean!
-          ) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
-
-        "Indicates an Input Object is a OneOf Input Object."
-        directive @oneOf on INPUT_OBJECT
-
-        "Directs the executor to skip this field or fragment when the `if` argument is true."
-        directive @skip(
-            "Skipped when true."
-            if: Boolean!
-          ) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
-
-        "Exposes a URL that specifies the behaviour of this scalar."
-        directive @specifiedBy(
-            "The URL that specifies the behaviour of this scalar."
-            url: String!
-          ) on SCALAR
-
-        type GqlAccount {
-          address: String!
-          "Month when account was closed. If not set means account is still open."
-          closedOn: GqlMonth
-          "Long description for the account."
-          description: String!
-          external: Boolean!
-          "Account id/name."
-          name: String!
-          "Account number. Can be null or unknown for external or proxy accounts."
-          number: String
-          "Month when account was open. Can be unknown."
-          openedOn: GqlMonth
-          "List of owner ids for this account."
-          owners: [String!]!
-          password: String!
-          "Replacement for type, so that accounts are grouped."
-          path: [String!]!
-          phone: String!
-          summary: Boolean!
-          url: String!
-          userName: String!
-        }
-
-        type GqlBalance {
-          amount: Int!
-          date: Date!
-          desc: String!
-          type: String!
-        }
-
-        type GqlStatement {
-          addSub: Int!
-          annualizedPercentChange: Float!
-          change: Int!
-          endBalance: GqlBalance
-          hasProjectedTransfer: Boolean!
-          inFlows: Int!
-          income: Int!
-          isClosed: Boolean!
-          isCovered: Boolean!
-          isProjectedCovered: Boolean!
-          month: GqlMonth!
-          name: String!
-          outFlows: Int!
-          percentChange: Float!
-          startBalance: GqlBalance
-          totalPayments: Int!
-          totalTransfers: Int!
-          transactions: [GqlTransaction!]!
-          unaccounted: Int!
-        }
-
-        type GqlSummaryData {
-          statements: [GqlStatement!]!
-          total: GqlSummaryStatement!
-        }
-
-        type GqlSummaryStatement {
-          accounts: [String!]!
-          addSub: Int!
-          annualizedPercentChange: Float!
-          change: Int!
-          endBalance: GqlBalance
-          inFlows: Int!
-          income: Int!
-          month: GqlMonth!
-          name: String!
-          outFlows: Int!
-          percentChange: Float!
-          startBalance: GqlBalance
-          totalPayments: Int!
-          totalTransfers: Int!
-          unaccounted: Int!
-        }
-
-        type GqlTable {
-          currentOwner: String!
-          months: [GqlMonth!]!
-          owners: [String!]!
-          rows: [GqlTableRow!]!
-        }
-
-        type GqlTableCell {
-          addSub: Int!
-          annualizedPercentChange: Float!
-          balance: Int
-          balanced: Boolean!
-          hasProjectedTransfer: Boolean!
-          isClosed: Boolean!
-          isCovered: Boolean!
-          isProjected: Boolean!
-          isProjectedCovered: Boolean!
-          month: GqlMonth!
-          percentChange: Float!
-          unaccounted: Int
-        }
-
-        type GqlTableRow {
-          account: GqlAccount!
-          cells: [GqlTableCell!]!
-          indent: Int!
-          isNormal: Boolean!
-          isSpace: Boolean!
-          isTotal: Boolean!
-          title: String!
-        }
-
-        type GqlTransaction {
-          balance: GqlBalance!
-          balanceFromStart: Int!
-          description: String!
-          isExpense: Boolean!
-          isIncome: Boolean!
-          toAccountName: String!
-        }
-
-        type Query {
-          hello: String!
-          "Returns a monthly statement for given account."
-          statement(account: String!, month: GqlMonth!, owner: String!): GqlStatement!
-          "Generates delta summary table between two months."
-          summary(accountType: String!, endMonth: GqlMonth!, owner: String!, startMonth: GqlMonth): GqlSummaryData!
-          "Generates full tally table in given month range."
-          table(endMonth: GqlMonth!, owner: String, startMonth: GqlMonth!): GqlTable!
-        }
-
-        "Date representation in YYYY-MM-DD format."
-        scalar Date
-
-        "Month representation in XxxYYYY format."
-        scalar GqlMonth
-        """.trimIndent()
+      var result = ""
       testApplication {
         configureTestClient(tallyPath)
 
         val response = client.get("/sdl")
         response.status shouldBe HttpStatusCode.OK
-        response.bodyAsText().trim() shouldBe expectedSchema
+        result = response.bodyAsText().trim()
       }
+      expectSelfie(result).toMatchDisk()
     }
 
     it("server should provide GraphiQL endpoint") {
