2a9bf5e28bc2884095494351d170d9a1bd091f95
arti <arthur@cherba.org>
2026-07-04 18:18:05 -0400

    kt server: pull out transaction build logic from BudgetBuilder into separate TransactionStatementBuilder.

--- server/kt/src/main/kotlin/com/cherba29/tally/data/builder/BudgetBuilder.kt
+++ server/kt/src/main/kotlin/com/cherba29/tally/data/builder/BudgetBuilder.kt
@@ -9,7 +9,6 @@ import com.cherba29.tally.core.Transfer
 import com.cherba29.tally.core.plus
 import com.cherba29.tally.data.Budget
 import com.cherba29.tally.statement.Statement
-import com.cherba29.tally.statement.TransactionStatement
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlin.collections.iterator
 import kotlin.collections.set
@@ -129,9 +128,12 @@ class BudgetBuilder(
 
     val (transactionStatementTable, elapsedTransactionTime) = timeSource.measureTimedValue {
       // TODO: this might throw due to so invariant being violated. Need to recover to previous state.
-      val transactionStatementTable = buildTransactionStatementTable(
-        months, leafToAccount,
-        leafToBalances, transfers)
+      val transactionStatementTable = TransactionTableBuilder().buildTransactionStatementTable(
+        months,
+        leafToAccount,
+        leafToBalances,
+        transfers
+      )
       for (stmt in transactionStatementTable) {
         nodeToStatement.getOrPut(stmt.treeNode) { mutableMapOf() }[stmt.monthRange.first] = stmt
       }
@@ -163,64 +165,6 @@ class BudgetBuilder(
       leafToAccount,
       nodeToStatement,
     )
-  }
-
-  fun buildTransactionStatementTable(
-    months: MonthRange,
-    leafToAccountMap: Map<TreeNode.Leaf, Account>,
-    leafToMonthlyBalancesMap: Map<TreeNode.Leaf, Map<Month, Balance>>,
-    leafToMonthlyTransfersMap: Map<TreeNode.Leaf, Map<Month, List<Transfer>>>
-  ): List<TransactionStatement> {
-    val statementTable = mutableListOf<TransactionStatement>()
-
-    // Working backwards.
-    val months = months.sortedDescending()
-    if (months.isEmpty()) {
-      throw IllegalArgumentException("Budget must have at least one month.")
-    }
-
-    for ((leafTreeNode, account) in leafToAccountMap) {
-      val accountStatements = mutableListOf<TransactionStatement>()
-      val monthlyTransfers = leafToMonthlyTransfersMap[leafTreeNode] ?: mapOf()
-      val monthlyBalances = leafToMonthlyBalancesMap[leafTreeNode] ?: mapOf()
-
-      // Make statement outside range so that its attributes relating to previous can be used.
-      val nextMonth = months.first().next()
-      var nextMonthStatement = TransactionStatementBuilder().fromTransfers(
-        leafTreeNode,
-        nextMonth..nextMonth,
-        account.isClosed(nextMonth),
-        monthlyTransfers[nextMonth],
-        monthlyBalances[nextMonth]
-      )
-      for (month in months) {
-        val statement = TransactionStatementBuilder().fromTransfers(
-          leafTreeNode,
-          month..month,
-          account.isClosed(month),
-          monthlyTransfers[month],
-          monthlyBalances[month]
-        )
-        statement.endBalance = nextMonthStatement.startBalance
-        statement.isCovered =
-          statement.endBalance == null || statement.endBalance!!.amount >= 0 || nextMonthStatement.coversPrevious
-        statement.isProjectedCovered = statement.isCovered || nextMonthStatement.coversProjectedPrevious
-        nextMonthStatement = statement
-        if (statement.startBalance != null) {
-          var prevBalance = statement.startBalance!!.amount
-          for (t in statement.transactions.asReversed()) {
-            t.balanceFromStart = prevBalance + t.balance.amount
-            prevBalance = t.balanceFromStart!!
-          }
-        }
-        accountStatements.add(statement)
-      }
-      // Do not include account if for all months it was closed.
-      if (accountStatements.any { !it.isClosed }) {
-        statementTable.addAll(accountStatements)
-      }
-    }
-    return statementTable
   }
 
   companion object {
---
+++ server/kt/src/main/kotlin/com/cherba29/tally/data/builder/TransactionTableBuilder.kt
@@ -0,0 +1,69 @@
+package com.cherba29.tally.data.builder
+
+import com.cherba29.tally.core.Account
+import com.cherba29.tally.core.Balance
+import com.cherba29.tally.core.Month
+import com.cherba29.tally.core.MonthRange
+import com.cherba29.tally.core.Transfer
+import com.cherba29.tally.core.TreeNode
+import com.cherba29.tally.statement.TransactionStatement
+
+class TransactionTableBuilder {
+  fun buildTransactionStatementTable(
+    months: MonthRange,
+    leafToAccountMap: Map<TreeNode.Leaf, Account>,
+    leafToMonthlyBalancesMap: Map<TreeNode.Leaf, Map<Month, Balance>>,
+    leafToMonthlyTransfersMap: Map<TreeNode.Leaf, Map<Month, List<Transfer>>>
+  ): List<TransactionStatement> {
+    val statementTable = mutableListOf<TransactionStatement>()
+
+    // Working backwards.
+    val months = months.sortedDescending()
+    if (months.isEmpty()) {
+      throw IllegalArgumentException("Budget must have at least one month.")
+    }
+
+    for ((leafTreeNode, account) in leafToAccountMap) {
+      val accountStatements = mutableListOf<TransactionStatement>()
+      val monthlyTransfers = leafToMonthlyTransfersMap[leafTreeNode] ?: mapOf()
+      val monthlyBalances = leafToMonthlyBalancesMap[leafTreeNode] ?: mapOf()
+
+      // Make statement outside range so that its attributes relating to previous can be used.
+      val nextMonth = months.first().next()
+      var nextMonthStatement = TransactionStatementBuilder().fromTransfers(
+        leafTreeNode,
+        nextMonth..nextMonth,
+        account.isClosed(nextMonth),
+        monthlyTransfers[nextMonth],
+        monthlyBalances[nextMonth]
+      )
+      for (month in months) {
+        val statement = TransactionStatementBuilder().fromTransfers(
+          leafTreeNode,
+          month..month,
+          account.isClosed(month),
+          monthlyTransfers[month],
+          monthlyBalances[month]
+        )
+        statement.endBalance = nextMonthStatement.startBalance
+        statement.isCovered =
+          statement.endBalance == null || statement.endBalance!!.amount >= 0 || nextMonthStatement.coversPrevious
+        statement.isProjectedCovered = statement.isCovered || nextMonthStatement.coversProjectedPrevious
+        nextMonthStatement = statement
+        if (statement.startBalance != null) {
+          var prevBalance = statement.startBalance!!.amount
+          for (t in statement.transactions.asReversed()) {
+            t.balanceFromStart = prevBalance + t.balance.amount
+            prevBalance = t.balanceFromStart!!
+          }
+        }
+        accountStatements.add(statement)
+      }
+      // Do not include account if for all months it was closed.
+      if (accountStatements.any { !it.isClosed }) {
+        statementTable.addAll(accountStatements)
+      }
+    }
+    return statementTable
+  }
+}
