5ce9ad791c9eeb3e0050f7277e1a316601488d65
arti <arthur@cherba.org>
2026-07-18 11:05:19 -0400

    web client: Extend jest test to web components to show that it does not work.

--- client/hbs/jest_config.js
+++ client/hbs/jest_config.js
@@ -1,11 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  "testEnvironment": "jest-environment-jsdom",
+  "preset": "ts-jest",
+  // Use a browser-like DOM environment for Web Components
+  "testEnvironment": "jsdom",
   "clearMocks": true,
   "collectCoverage": true,
   "coverageDirectory": "coverage",
   "coverageProvider": "v8",
-  "testRegex": [".*\\.test.ts$"],
   "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+    // Compile TypeScript using ts-jest
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+  },
+  // Force Jest to process Lit ESM files inside node_modules
+  "transformIgnorePatterns": [
+    "node_modules/(?!(lit|lit-html|lit-element|@lit|@lit-labs)/)",
+  ],
 }
--- client/hbs/src/components/account-tooltip.test.ts
+++ client/hbs/src/components/account-tooltip.test.ts
@@ -1,7 +1,30 @@
-import { describe, expect, test } from '@jest/globals';
+import { afterEach, beforeEach, describe, expect, it } from '@jest/globals';
+import { AccountTooltip } from './account-tooltip';
+import {Account} from '@tally/lib/core/account';
 
-describe('Creation', () => {
-  test('basic', () => {
-    expect('test-account1').toBe('test-account1');
+describe('AccountTooltip component', () => {
+  let element: AccountTooltip;
+
+  beforeEach(()=> {
+    // Create and append the component to the jsdom body
+    element = document.createElement('account-tooltip') as AccountTooltip;
+    document.body.appendChild(element);
+  });
+
+  afterEach(() => {
+    // Clean up DOM after each test execution
+    element.remove();
+  });
+
+  it('renders with default property values', async () => {
+    element.account = {
+      name: "test-account1"
+    } as Account;
+    // Wait for Lit's async rendering lifecycles to finish
+    await element.updateComplete;
+
+    // Drill into the Shadow DOM to verify content.
+    // TODO: use recomended https://lit.dev/docs/tools/testing/.
+    expect(element.shadowRoot).toBeNull();
   });
 });
